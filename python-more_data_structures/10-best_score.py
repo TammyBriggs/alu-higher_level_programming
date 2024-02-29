ec0d16057293e6a7e8b0f2c28113a171ca9755fd
TammyBriggs <t.briggs1@alustudent.com>
2024-02-29 13:15:16 -0800

    Update

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+# This script IS a function that returns the key with the biggest value.
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
-    return (max(a_dictionary,key=a_dictionary.get))
+
+    return (max(a_dictionary, key=a_dictionary.get))
