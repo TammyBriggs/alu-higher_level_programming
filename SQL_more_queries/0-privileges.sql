c4c0f2b380b153abb91856bb7921e1a105a346ca
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2024-03-22 08:09:22 +0200

    Assignment complete

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,7 @@
 -- Script that lists all privileges of the MySQL users
+
 -- Query to list all privileges (GRANT) of the MySQL users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT * 
+FROM information_schema.user_privileges
+WHERE grantee IN ('user_0d_1@localhost', 'user_0d_2@localhost');
+
